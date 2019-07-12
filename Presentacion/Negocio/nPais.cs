9a4ce7fdad8bfecd907966733d072650d5d5d356
jaguar009 <jaguar009@gmail.com>
2019-07-12 14:18:46 -0500

    Final

---
+++ Presentacion/Negocio/nPais.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidades;
+using Datos;
+
+namespace Negocio
+{
+    public class nPais
+    {
+        dPais paisdatos;
+        public nPais()
+        {
+            paisdatos = new dPais();
+        }
+        public string RegistrarPais(string nombrepais)
+        {
+            ePais pais = new ePais()
+            {
+                Nombrepais = nombrepais
+            };
+            return paisdatos.Insertar(pais);
+        }
+        public string Modificarpais(int idpais, string nombrepais)
+        {
+            ePais pais = new ePais()
+            {
+                Idpais = idpais,
+                Nombrepais = nombrepais
+            };
+            return paisdatos.Modificar(pais);
+        }
+
+        public string Eliminarpais(int id)
+        {
+            return paisdatos.Eliminar(id);
+        }
+
+        public List<ePais> Listarpais()
+        {
+            return paisdatos.ListarTodo();
+        }
+    }
+}
---
+++ Presentacion/Negocio/nPasajero.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Datos;
+namespace Negocio
+{
+    public class nPasajero
+    {
+        dPasajero pasajerodatos;
+
+        public nPasajero()
+        {
+            pasajerodatos = new dPasajero();
+        }
+
+        public string Registrarpasajero(string nombrepasajero, string apellido, int idpais)
+        {
+            ePais pais = new ePais()
+            {
+                Idpais = idpais
+            };
+
+            ePasajero pasajero = new ePasajero()
+            {
+                Nombre = nombrepasajero,
+                Apellido = apellido,
+                pais = pais
+            };
+
+            return pasajerodatos.Insertar(pasajero);
+        }
+
+        public string Modificarpasajero(int idpasajero, string nombrepasajero, string apellido, int idpais)
+        {
+            ePais pais = new ePais()
+            {
+                Idpais = idpais
+            };
+
+            ePasajero pasajero = new ePasajero()
+            {
+                Idpasajero = idpasajero,
+                Nombre = nombrepasajero,
+                Apellido = apellido,
+                pais = pais
+            };
+            return pasajerodatos.Modificar(pasajero);
+        }
+
+        public string Eliminarpasajero(int id)
+        {
+            return pasajerodatos.Eliminar(id);
+        }
+
+        public List<ePasajero> Listarpasajero()
+        {
+            return pasajerodatos.ListarTodo();
+        }
+    }
+}
